f0707b31d569d0848d9422a4674e7a10d7ceb8cf
furkanGit <furkancaliskan9801@gmail.com>
2022-11-15 18:00:45 +0100

    Palidroma Done

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,3 +1,32 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 //Dare output relativo.
+
+
+
+let wordUser = prompt("Inserisci una parola");
+console.log("Inserisci una parola:", wordUser);
+
+let parolaInvertita = reverseString(wordUser);
+
+    function reverseString(world) {
+
+    let splitword = world.split("");
+    console.log("La parola divisa è:", splitword);
+
+    splitword = splitword.reverse();
+    console.log("La parola invertita è:", splitword);
+
+
+    wordReverse = splitword.join("");
+    console.log("La parola finale è:", splitword);
+
+    return wordReverse;
+
+}
+
+if(wordUser == parolaInvertita){
+    console.log(`La parola ${wordUser} è palindroma`);
+    } else {
+    console.log(`La parola ${wordUser} non è palindroma`);
+    }
